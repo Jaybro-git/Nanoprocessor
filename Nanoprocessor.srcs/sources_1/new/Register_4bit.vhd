3db87877bf1746ad34e37c76e585b2e7c5bf42aa
Jaybro-git <janithmahanama47@gmail.com>
2025-05-12 16:17:33 +0530

    Second commit

---
+++ Nanoprocessor.srcs/sources_1/new/Register_4bit.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/12/2025 04:14:15 PM
+-- Design Name: 
+-- Module Name: Register_4bit - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Register_4bit is
+    Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
+           EN : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           Reset : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR (3 downto 0));
+end Register_4bit;
+
+architecture Behavioral of Register_4bit is
+
+begin
+
+    process (Clk,EN,Reset) begin
+        if Reset = '0' then
+            if (rising_edge(Clk)) then
+                 if EN = '1' then
+                    Q <= D;
+                 end if;
+            end if;
+        else
+            Q <= "0000";
+        end if;
+    end process;
+      
+end Behavioral;
